3a2a0354d8880ddc8ad05b950afbe79a5af989a1
sadaqat <sadaqat.eshaafi@gmail.com>
2023-02-22 15:22:39 +0500

    FIRST_COMMIT

---
+++ app/src/main/java/com/google/android/gms/example/interstitialexample/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.google.android.gms.example.interstitialexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
